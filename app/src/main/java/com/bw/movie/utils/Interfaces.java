f80d53734c8d3a8160ca6cc5e2b6715ce66140e2
liangjian <1187413406@qq.com>
2019-01-26 10:05:38 +0800

    interfaces 完成

--- app/src/main/java/com/bw/movie/utils/Interfaces.java
+++ app/src/main/java/com/bw/movie/utils/Interfaces.java
@@ -1,5 +1,7 @@
 package com.bw.movie.utils;
 
+import org.w3c.dom.Comment;
+
 /**
  * <p>文件描述：接口文档<p>
  * <p>作者：${adai}<p>
@@ -19,7 +21,7 @@ public class Interfaces {
     //    登陆 post
     public static final String Land = "movieApi/user/v1/login";
     //    查询会员首页信息  我的信息  get  得到当前用户关注的影院  电影
-    public static final String MyMessage = "movieApi/user/v1/login";
+    public static final String MyMessage = "movieApi/user/v1/verify/findUserHomeInfo";
     //    修改信息 post
     public static final String ModifyInformation = "movieApi/user/v1/verify/modifyUserInfo";
     //    上传头像 post
@@ -54,9 +56,71 @@ public class Interfaces {
     public static final String SearchForRecommendedCinema = "movieApi/cinema/v1/findRecommendCinemas";
     //    查询热门电影 get
     public static final String SearchForHotMovies = "movieApi/movie/v1/findHotMovieList";
-    
+
     public static final String UpdataPwd = "movieApi/user1erify/modifyUserPwd";
 
     public static final String UserMessage = "movieApi/user1erify/getUserInfoByUserId";
-    
+
+
+//查询用户 关注的影片列表 get
+
+    public static final String SearchForVideosOfInterestToUsers = "movieApi/movie/v1/verify/findMoviePageList";
+    //    关注电影 get
+    public static final String FocusOnMovies = "movieApi/movie/v1/verify/followMovie";
+    //    取消关注电影 get
+    public static final String CancelFilmConcern = "movieApi/movie/v1/verify/cancelFollowMovie";
+    //    查询影片评论 get
+    public static final String SearchForFilmReviews = "movieApi/movie/v1/findAllMovieComment";
+    //添加用户对影片的评论 post
+    public static final String AddUserCommentsOnTheFilm = "movieApi/movie/v1/verify/movieComment";
+    //查询影片评论回复 get
+    public static final String InquiryForFilmCommentReply = "movieApi/movie/v1/findCommentReply";
+
+    //    添加用户对评论的回复 post
+    public static final String AddUserResponsesToComments = "movieApi/movie/v1/verify/commentReply";
+    //    电影评论点赞 post
+    public static final String CommentsOnTheFilm = "movieApi/movie/v1/verify/movieCommentGreat";
+    //    根据影院ID查询该影院当前排期的电影列表 get
+    public static final String QueryCurrentMoviesBasedOnTheaterID = "movieApi/movie/v1/findMovieListByCinemaId";
+    //    根据电影ID和影院ID查询电影排期列表 get
+    public static final String SearchMovieSchedulesBasedOnMovieIDAndCinemaID = "movieApi/movie/v1/findMovieScheduleList";
+    //    根据电影ID查询当前排片该电影的影院列表 get
+    public static final String QuerAyTheListOfCinemasCurrentlyInProductionAccordingToTheMovieID = "movieApi/movie/v1/findCinemasListByMovieId";
+    //    购票查询 post
+    public static final String TicketEnquiry = "movieApi/movie/v1/verify/buyMovieTicket";
+    //    支付 post
+    public static final String Payment = "movieApi/movie/v1/verify/pay";
+    //    根据影院ID查询该影院下即将上映的电影列表 get
+    public static final String QueryTheListOfUpcomingMoviesInTheCinemaBasedOnTheCinemaID = "movieApi/movie/v1/findSoonMovieByCinemaId";
+    //    查询电影信息明细 GET
+    public static final String InquiryFilmInformationDetails = "movieApi/cinema/v1/findCinemaInfo";
+    //    根据电影名称模糊查询电影院 GET
+    public static final String InquireTheCinemaVaguelyAccordingToTheNameOfTheFilm = "movieApi/cinema/v1/findAllCinemas";
+    //    .查询用户关注的影院信息get
+    public static final String SearchForCinemaInFormationThatUsersAreInterestedIn = "movieApi/cinema/v1/verify/findCinemaPageList";
+    //    关注影院 get
+    public static final String CinemaAttention = "movieApi/cinema/v1/verify/followCinema";
+    //取消关注影院 GET
+    public static final String CancelAttentionToCinema = "movieApi/cinema/v1/verify/cancelFollowCinema";
+    //    查询影院用户评论列表 get
+    public static final String QueryTheListOfCinemaUserReviews = "movieApi/cinema/v1/findAllCinemaComment";
+    //    影院评论 post
+    public static final String CinemaReviews = "movieApi/cinema/v1/verify/cinemaComment";
+    //    影院评论点赞 post
+    public static final String CinemaComments = "movieApi/cinema/v1/verify/cinemaCommentGreat";
+    //    意见反馈 post
+    public static final String Feedback = "movieApi/tool/v1/verify/recordFeedBack";
+    //    查询新版本 get
+    public static final String QueryForNewVersion = "movieApi/tool/v1/findNewVersion";
+    //    查询系统消息列表 get
+    public static final String QuerySystemMessageList = "movieApi/tool/v1/verify/findAllSysMsgList";
+    //    系统消息读取状态修改 get
+    public static final String SystemMessageReadStatusModification = "movieApi/tool/v1/verify/changeSysMsgStatus";
+    //    查询用户当前未读消息数量 get
+    public static final String QueryTheNumberOfUnreadMessagesTheUserCurrentlyHas = "movieApi/tool/v1/verify/findUnreadMessageCount";
+    //    上传消息推送使用的token post
+    public static final String TokenForUploadingMessagePush = "movieApi/tool/v1/verify/uploadPushToken";
+    //    微信分享前置接口，获取分享所需参数 get
+    public static final String WeChatShare = "movieApi/tool/v1/wxShare";
+
 }
