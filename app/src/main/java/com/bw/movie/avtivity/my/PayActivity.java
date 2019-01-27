7b155d83f11eb3d572e3370edc7d4069e5ffeda9
fuxiaor <fxr0905@163.com>
2019-01-27 18:46:34 +0800

    myhorn

--- app/src/main/java/com/bw/movie/avtivity/my/PayActivity.java
+++ app/src/main/java/com/bw/movie/avtivity/my/PayActivity.java
@@ -1,16 +1,20 @@
 package com.bw.movie.avtivity.my;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bw.movie.R;
 import com.bw.movie.base.BaseActivity;
 
-public class PayActivity extends BaseActivity {
+public class PayActivity extends BaseActivity implements View.OnClickListener {
 
     private ImageView pay_back;
+    private TextView no_pay;
+    private TextView paid;
+
 
     @Override
     protected void initView() {
@@ -20,13 +24,12 @@ public class PayActivity extends BaseActivity {
     @Override
     protected void initData() {
         pay_back = findViewById(R.id.Pay_Back);
+        no_pay = findViewById(R.id.No_pay);
+        paid = findViewById(R.id.Paid);
 
-        pay_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        pay_back.setOnClickListener(this);
+        no_pay.setOnClickListener(this);
+        paid.setOnClickListener(this);
     }
 
     @Override
@@ -34,4 +37,24 @@ public class PayActivity extends BaseActivity {
 
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Pay_Back:
+                finish();
+                break;
+            case R.id.No_pay:
+                no_pay.setBackgroundResource(R.drawable.cinema_ed);
+                no_pay.setTextColor(Color.WHITE);
+                paid.setBackgroundResource(R.drawable.cinema_noed);
+                paid.setTextColor(Color.BLACK);
+                break;
+            case R.id.Paid:
+                paid.setBackgroundResource(R.drawable.cinema_ed);
+                paid.setTextColor(Color.WHITE);
+                no_pay.setBackgroundResource(R.drawable.cinema_noed);
+                no_pay.setTextColor(Color.BLACK);
+                break;
+        }
+    }
 }
